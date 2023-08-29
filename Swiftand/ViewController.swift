5bd5b1ce76455e4d6be74c9162b81502ba726821
Gabriel Mors <gabrielmors210@gmail.com>
2023-08-29 17:03:03 -0400

    configuracao inicial do projeto

---
+++ Swiftand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swiftand
+//
+//  Created by Gabriel Mors  on 29/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
